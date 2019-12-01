d97bc3d0b33b445ea2a87f5b055b0346ab4dd548
egyptianf <walied2@protonmail.com>
2019-12-01 17:09:05 +0200

    splitting the data for training and testing in a range of 30% to 70%

--- main.py
+++ main.py
@@ -10,25 +10,20 @@ if __name__ == '__main__':
 
     preprocessing = Preprocessing()
     preprocessing.assume_missing_values(data)
+    for percent in range(3, 8):
+        training_data, testing_data = preprocessing.split_into_training_and_testing(data, percent/float(10))
+        attributes_number = len(training_data[0]) - 1
+        decision_tree = DecisionTree()
+        root_node = decision_tree.build(training_data)
+        # decision_tree.print()
+        # print("Classification: ")
+        accuracy = 0
+        for row in testing_data:
+            classified = decision_tree.classify(row, decision_tree.root)
+            classified.calc_percentages(len(testing_data))
+            if classified.republicans_percent > 50.0 and row[0] == 'republican' or (
+                    classified.democrats_percent > 50.0 and row[0] == 'democrat'):
+                accuracy += 1
 
-    training_data, testing_data = preprocessing.split_into_training_and_testing(data, 0.6)
-    attributes_number = len(training_data[0]) - 1
-
-    decision_tree = DecisionTree()
-    root_node = decision_tree.build(training_data)
-    decision_tree.print()
-    print("Classification: ")
-    classified = decision_tree.classify(training_data[0], decision_tree.root)
-    classified.calc_percentages(len(training_data))
-    print(classified.str_with_probability())
-
-    accuracy = 0
-    for row in testing_data:
-        classified = decision_tree.classify(row, decision_tree.root)
-        classified.calc_percentages(len(testing_data))
-        if classified.republicans_percent > 50.0 and row[0] == 'republican' or (
-                classified.democrats_percent > 50.0 and row[0] == 'democrat'):
-            accuracy += 1
-
-    accuracy = accuracy / float(len(testing_data))
-    print("Accuracy: ", accuracy)
+        accuracy = accuracy / float(len(testing_data))
+        print("Accuracy using training data", percent/float(10)*100, "% is: ", accuracy)
