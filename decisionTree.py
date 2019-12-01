119340af966bc84b6260e0bacdf3635616fd943a
egyptianf <walied2@protonmail.com>
2019-12-01 03:14:48 +0200

    finished: build function, classifiy function and other utility functions, tested on test_set.

--- decisionTree.py
+++ decisionTree.py
@@ -1,36 +1,67 @@
 import math
 
 
-class Node:
+def label_split(dataset):
+    yes_counter, no_counter = 0, 0
+    for row in dataset:
+        if row[0] == 'democrat':
+            yes_counter += 1
+        elif row[0] == 'republican':
+            no_counter += 1
+    return yes_counter, no_counter
+
+
+class DecisionNode:
     decision_attribute = None
-    dataset = []
     left_subset = []
     right_subset = []
 
-    def __init__(self, decision_attr, dataset, left_subset, right_subset):
+    def __init__(self, decision_attr, left_subset, right_subset):
         self.decision_attribute = decision_attr
-        self.dataset = dataset
         self.left_subset = left_subset
         self.right_subset = right_subset
 
 
-class DecisionTree:
+class Leaf:
+    original_dataset_size = 0
+    democrats_percent = 0
+    republicans_percent = 0
 
-    def label_split(self, dataset):
-        yes_counter, no_counter = 0, 0
-        for row in dataset:
-            if row[0] == 'democrat':
-                yes_counter += 1
-            elif row[0] == 'republican':
-                no_counter += 1
-        return yes_counter, no_counter
+    def __init__(self, dataset):
+        self.democrats_predictions, self.republicans_predictions = label_split(dataset)
+
+    def calc_percentages(self, original_dataset_size):
+        self.original_dataset_size = original_dataset_size
+        self.democrats_percent = round(self.democrats_predictions / float(self.original_dataset_size) * 100, 2)
+        self.republicans_percent = round(self.republicans_predictions / float(self.original_dataset_size) * 100, 2)
+
+    def str(self) -> str:
+        whole_str = ""
+        if self.democrats_predictions != 0:
+            whole_str += "democrats: " + str(self.democrats_predictions)
+        if self.republicans_predictions != 0:
+            whole_str += "republicans: " + str(self.republicans_predictions)
+        return whole_str
+
+    def str_with_probability(self) -> str:
+        whole_str = ""
+        if self.democrats_predictions != 0:
+            whole_str += "democrats: " + str(self.democrats_percent) + "% "
+        if self.republicans_predictions != 0:
+            whole_str += "republicans: " + str(self.republicans_percent) + "%"
+
+        return whole_str
+
+
+class DecisionTree:
+    root = None
 
     def entropy(self, dataset) -> float:
         """
         :param dataset: list
         :return: entropy: float
         """
-        democrats_counter, republicans_counter = self.label_split(dataset)
+        democrats_counter, republicans_counter = label_split(dataset)
         dataset_len = float(len(dataset))
         democrats_prob = democrats_counter / dataset_len
         republicans_prob = republicans_counter / dataset_len
@@ -66,15 +97,64 @@ class DecisionTree:
         return H - (len(yes_subset) / float(len(subset)) * self.entropy(yes_subset)) - (
                 len(no_subset) / float(len(subset)) * self.entropy(no_subset))
 
-    def get_best_attr_to_split_on(self, data, attributes_number) -> int:
+    def get_best_attr_to_split_on(self, data, attributes_number) -> 'integer, float':
         best_attribute = 1  # best attribute number and its gain
         best_gain = 0
         for i in range(1, attributes_number + 1):
+            true_subset, false_subset = self.attribute_split(data, i)
+
+            # Skip this split if it does not divide the dataset
+            if len(true_subset) == 0 or len(false_subset) == 0:
+                continue
+
             if self.information_gain(data, i) > best_gain:
                 best_attribute = i
                 best_gain = self.information_gain(data, i)
 
-        return best_attribute
+        return best_attribute, best_gain
 
     def build(self, dataset):
-        pass
+        attributes_number = len(dataset[0]) - 1
+        # Will return attribute number and corresponding gain
+        A, gain = self.get_best_attr_to_split_on(dataset, attributes_number)
+
+        if gain == 0:
+            return Leaf(dataset)
+
+        true_subset, false_subset = self.attribute_split(dataset, A)
+
+        true_branch = self.build(true_subset)
+        false_branch = self.build(false_subset)
+        self.root = DecisionNode(A, true_branch, false_branch)
+        return self.root
+
+    def print_tree(self, node, spacing=""):
+        # Base case
+        if isinstance(node, Leaf):
+            whole_str = spacing + "Predict "
+            whole_str += node.str()
+            print(whole_str)
+            return
+
+        print(spacing + "Feature:", node.decision_attribute, "?")
+
+        # Call recursively on the true and false branches
+        print(spacing + "--> True")
+        self.print_tree(node.left_subset, spacing + " ")
+        print(spacing + "--> False")
+        self.print_tree(node.right_subset, spacing + " ")
+
+    def print(self):
+        self.print_tree(self.root)
+
+    def classify(self, row, node):
+        # Base case: we've reached a leaf
+        if isinstance(node, Leaf):
+            return node
+
+        # Decide whether to follow the true branch or the false branch
+        # Compare the feature stored in the node to example we're considering
+        if row[node.decision_attribute] == 'y':
+            return self.classify(row, node.left_subset)
+        else:
+            return self.classify(row, node.right_subset)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fileIO import FileIO
 from preprocess import Preprocessing
 from decisionTree import DecisionTree
 
+
 if __name__ == '__main__':
     filename = 'house-votes-84.data.txt'
     fileio = FileIO()
@@ -10,10 +11,24 @@ if __name__ == '__main__':
     preprocessing = Preprocessing()
     preprocessing.assume_missing_values(data)
 
-    training_data, testing_data = preprocessing.split_into_training_and_testing(data, 0.3)
+    training_data, testing_data = preprocessing.split_into_training_and_testing(data, 0.6)
     attributes_number = len(training_data[0]) - 1
-    print("training data length: ", len(training_data))
-    tree = DecisionTree()
 
-    # Suppose we want to see the information gain for all attributes
-    print("best attribute to split on is: ", tree.get_best_attr_to_split_on(training_data, attributes_number))
+    decision_tree = DecisionTree()
+    root_node = decision_tree.build(training_data)
+    decision_tree.print()
+    print("Classification: ")
+    classified = decision_tree.classify(training_data[0], decision_tree.root)
+    classified.calc_percentages(len(training_data))
+    print(classified.str_with_probability())
+
+    accuracy = 0
+    for row in testing_data:
+        classified = decision_tree.classify(row, decision_tree.root)
+        classified.calc_percentages(len(testing_data))
+        if classified.republicans_percent > 50.0 and row[0] == 'republican' or (
+                classified.democrats_percent > 50.0 and row[0] == 'democrat'):
+            accuracy += 1
+
+    accuracy = accuracy / float(len(testing_data))
+    print("Accuracy: ", accuracy)
